03a57b05ef069ddd156bfd830a0f2f1a6ac3735c
vick3d <viktoro0090@gmail.com>
2019-01-22 07:55:53 -0500

    Add account as instance_double, Add user stories to README

--- lib/atm.rb
+++ lib/atm.rb
@@ -10,4 +10,5 @@ class ATM
     end
 
 
+    
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,13 @@
 require './lib/atm.rb'
 
 describe ATM do
+    let(:account) { instance_double('Account') }
+
+    before do
+        allow(account).to receive(:balance).and_return(100)
+        allow(account).to receive(:balance=)
+    end
+
     it'has $1000 on init' do
         expect(subject.funds).to eq 1000
     end
@@ -9,5 +16,5 @@ describe ATM do
         subject.withdraw 50
         expect(subject.funds).to eq 950
     end
-    
+   
 end
