b677b5581d27db3d00dd10dadf24be3b920a6018
vick3d <viktoro0090@gmail.com>
2019-01-24 13:53:53 +0100

    Add status and expiry date on Initialize and add deactivation method

--- lib/account.rb
+++ lib/account.rb
@@ -1,8 +1,19 @@
 class Account 
-attr_accessor :pin_code, :balance, :account_status, :exp_date, :account_owner
+    attr_accessor :pin_code, :balance, :account_status, :exp_date, :account_owner  
+    STANDARD_VALIDITY_YRS = 5
 
     def initialize
     @pin_code = rand(1000..9999)
-end
+    @exp_date = set_expire_date
+    @account_status = :active
+    end
+
+    def set_expire_date
+        Date.today.next_year(STANDARD_VALIDITY_YRS).strftime('%m/%y')
+    end
+
+    def deactivate
+        @account_status = :deactivated
+    end
 
 end
